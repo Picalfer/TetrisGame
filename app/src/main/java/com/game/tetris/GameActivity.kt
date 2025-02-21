66039537eb44d7bcd770b0552d5b3c9d4d0ea0bb
Arthur Kozhemyakin <trable228@icloud.com>
2025-02-21 15:56:17 +0300

    xml for game activity

--- app/src/main/java/com/game/tetris/GameActivity.kt
+++ app/src/main/java/com/game/tetris/GameActivity.kt
@@ -8,11 +8,24 @@ import com.game.tetris.databinding.ActivityGameBinding
 class GameActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityGameBinding
+    private var appPreferences: AppPreferences? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityGameBinding.inflate(layoutInflater)
         enableEdgeToEdge()
         setContentView(binding.root)
+        appPreferences = AppPreferences(this)
+
+        updateHighScore()
+        updateCurrentScore()
+    }
+
+    private fun updateHighScore() {
+        binding.tvHighScore.text = appPreferences?.getHighScore().toString()
+    }
+
+    private fun updateCurrentScore() {
+        binding.tvCurrentScore.text = "0"
     }
 }
